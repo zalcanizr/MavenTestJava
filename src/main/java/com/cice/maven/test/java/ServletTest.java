c5afe2d3365caf8d8e7da08c6873c72d1d0fa9e9
Formador <Formador@aula8PovMacForm.local>
2018-09-12 21:25:31 +0200

    Initial commit

---
+++ src/main/java/com/cice/maven/test/java/ServletTest.java
@@ -0,0 +1,22 @@
+package com.cice.maven.test.java;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class ServletTest extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h1>");
+        writer.println("Hello World from Servlet!!!!");
+        writer.println("</h1>");
+
+    }
+}
