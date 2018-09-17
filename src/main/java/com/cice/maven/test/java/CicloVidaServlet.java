dd3ab59307b1273636a22cbae27d65bb9f0245bd
cice <cice@A8POVMac10.local>
2018-09-17 21:24:38 +0200

    cambios sesion lunes 17 sept mod last

--- src/main/java/com/cice/maven/test/java/CicloVidaServlet.java
+++ src/main/java/com/cice/maven/test/java/CicloVidaServlet.java
@@ -1,4 +1,36 @@
 package com.cice.maven.test.java;
 
-public class CicloVidaServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CicloVidaServlet extends HttpServlet{
+
+
+
+    @Override
+    public void init() throws ServletException {
+        super.init();//esto aparece al añadirlo y llama al metodo init de la clase padre pero no me interesa
+        System.out.println("INICIANDO SERVLET...");
+    }
+
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.service(req, resp);
+        System.out.println("LLAMANDO AL SERVICE DEL SERVLET...");
+    }
+
+
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("DESTRUYENDO SERVLET...");
+    }
+
+
 }
